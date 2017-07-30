3bd380107e20eaae913248fb7ec8f669a187c56b
kaitoii11 <kaitoii1111@gmail.com>
2017-07-31 06:58:21 +0900

    updated html and start adding template html

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
 from django.http import HttpResponse
-
+from .models import article
 
 # Create your views here.
 
 def index(request):
     return render_to_response('news/index.html')
 
+def index2(request):
+    return render_to_response('news/index_template.html')
 
-# def article(request, pk):
+
+def article(request, id):
 #    post = get_object_or_404()
 #    return render(request, 'news/detail.html', {'post':post})
-def article(request):
     return render_to_response('news/article.html')
+
+def article2(request, id):
+    return render_to_response('news/article_template.html', {'article':article})
