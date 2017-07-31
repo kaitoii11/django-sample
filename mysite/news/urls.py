0f3e7067f4f2c1573446d26f2408c565ca1ae5ed
Kaito Ii <kaitoi1111@gmail.com>
2017-07-31 07:31:13 +0000

    updated urls to have namespace

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^top$', views.index2, name='top'),
 #    url(r'^article/$', views.article, name='article'),
     url(r'^article/(?P<id>\d+)$', views.article, name='articles'),
-    url(r'^example/(?P<id>\d+)$', views.article2, name='articles'),
+    url(r'^example/(?P<id>\d+)$', views.article2, name='articles2'),
 ]
