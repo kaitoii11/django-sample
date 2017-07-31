e9c61fc24533665604562a06bda57220db3b8a39
Kaito Ii <kaitoi1111@gmail.com>
2017-07-31 04:25:47 +0000

    migrate database to postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,13 +77,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #},
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'postgres': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.s'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test1',
+	'USER': 'user01',
+	'PASSWORD': 'passwd',
+	'HOST': '10.2.3.4',
+	'PORT': 5432
     }
 }
 
